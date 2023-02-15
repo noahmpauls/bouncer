0e4a54f09a37fca46dc8bc803a94d64342c49dee
Noah Pauls <noahmpauls@gmail.com>
2023-02-15 14:30:21 -0800

    feat!: record viewing time by host

--- timer.js
+++ timer.js
@@ -1,46 +1,66 @@
-// document.body.style.border = "5px solid red";
-
+// Get the host and record the visit to it
 let host = window.location.host;
 console.log(host);
+recordLastSite(host);
 
-browser.storage.local.get(host)
-  .then(time => {
-    console.log("retrieved from storage");
-    console.log(time);
-  });
-
-let hostTime = {};
-hostTime[host] = Date.now();
-browser.storage.local.set(hostTime)
-  .then(() => console.log("time is set."));
-
-window.addEventListener("beforeunload", () => {
-  console.log("before unload: setting last site")
-  recordLastSite(host);
-})
-
-browser.storage.local.get("lastSite")
-  .then(value => {
-    console.log("got last site.")
-    console.log(value)
-  });
-
-function recordLastSite(site) {
-  const addSite = prev => {
-    if (prev === undefined) {
-      return [site];
-    } else {
-      let next = [...prev];
-      next.push(site);
-      return next;
-    }
-  }
-  
-  modifyStorage("lastSite", addSite);
+// Start timing the current page view
+let viewStartTime = undefined;
+if (document.visibilityState === "visible") {
+  setViewStartTime();
 }
 
-function modifyStorage(key, update) {
-  browser.storage.local.get(key)
+window.addEventListener("visibilitychange", event => {
+  if (document.visibilityState === "visible") {
+    setViewStartTime();
+  } else if (document.visibilityState === "hidden") {
+    setSiteViewTime();
+  }
+});
+
+window.addEventListener("pageshow", event => {
+  if (document.visibilityState === "visible") {
+    setViewStartTime();
+  }
+});
+
+window.addEventListener("pagehide", event => {
+  setSiteViewTime();
+});
+
+function recordLastSite(site) {
+  modifyStorage("lastSite", [], prev => {
+    let next = [...prev];
+    next.push(site);
+    return next;
+  });
+}
+
+function setViewStartTime() {
+  console.log("start view time");
+  viewStartTime = Date.now();
+}
+
+function setSiteViewTime() {
+  console.log("end view time");
+  if (viewStartTime === undefined || viewStartTime === null) {
+    console.error(`cannot record site view time when start time is ${viewStarTime}`)
+    return
+  }
+
+  const viewTime = Date.now() - viewStartTime;
+  
+  modifyStorage(`viewTime:${host}`, 0, prev => {
+    console.log(prev);
+    return prev + viewTime;
+  });
+}
+
+function modifyStorage(key, defaultValue = null, update) {
+  let getArg = key;
+  if (defaultValue !== null) {
+    getArg = { [key]: defaultValue };
+  }
+  browser.storage.local.get(getArg)
     .then(value => {
       value[key] = update(value[key]);
       return value;
