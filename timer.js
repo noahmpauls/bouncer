22cc9f2ea3c34324c3d64ed924299205f6072161
Noah Pauls <noahmpauls@gmail.com>
2023-02-15 11:37:34 -0800

    feat!: advanced storage update on beforeunload
    Introduce a "beforeunload" listener that performs an action when
    navigating away.
    
    Introduce a method for updating storage values based on previous state.

--- timer.js
+++ timer.js
@@ -1,18 +1,51 @@
-document.body.style.border = "5px solid red";
-let path = window.location.host;
-console.log(path);
+// document.body.style.border = "5px solid red";
 
-browser.storage.local.get(path)
+let host = window.location.host;
+console.log(host);
+
+browser.storage.local.get(host)
   .then(time => {
     console.log("retrieved from storage");
     console.log(time);
   });
 
 let hostTime = {};
-hostTime[path] = Date.now();
+hostTime[host] = Date.now();
 browser.storage.local.set(hostTime)
   .then(() => console.log("time is set."));
 
-window.onbeforeunload(async () => {
+window.addEventListener("beforeunload", () => {
+  console.log("before unload: setting last site")
+  recordLastSite(host);
+})
 
-});
+browser.storage.local.get("lastSite")
+  .then(value => {
+    console.log("got last site.")
+    console.log(value)
+  });
+
+function recordLastSite(site) {
+  const addSite = prev => {
+    if (prev === undefined) {
+      return [site];
+    } else {
+      let next = [...prev];
+      next.push(site);
+      return next;
+    }
+  }
+  
+  modifyStorage("lastSite", addSite);
+}
+
+function modifyStorage(key, update) {
+  browser.storage.local.get(key)
+    .then(value => {
+      value[key] = update(value[key]);
+      return value;
+    })
+    .then(value => {
+      browser.storage.local.set(value);
+    });
+}
