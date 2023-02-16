f9d1b7590db4a0543ce81072464b7dd4aaf677b2
Noah Pauls <noahmpauls@gmail.com>
2023-02-16 10:43:12 -0800

    feat!: enforce rules defined on settings page

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -1,30 +1,37 @@
-const rulesDiv = document.getElementById("rules");
-
-browser.storage.local.get({ rules: [] })
-  .then(data => {
-    rulesDiv.children = [];
-    rules = data.rules;
-    for (const rule of rules) {
-      const ruleElement = document.createElement("p");
-      ruleElement.innerText = `${rule.host}: ${rule.milliseconds}`;
-      rulesDiv.appendChild(ruleElement);
-    }
-  });
+refreshRuleDisplay();
 
 const ruleForm = document.getElementById("add-rule");
+const hostInput = document.getElementById("host");
+const millisecondsInput = document.getElementById("milliseconds");
 
 ruleForm.addEventListener("submit", event => {
   event.preventDefault();
-  const data = new FormData(event.target);
-  const dataObject = {};
-  for (const field of data) {
-    const value = field[0] === "milliseconds" ? parseInt(field[1]) : field[1];
-    dataObject[field[0]] = value;
-  }
+  const dataObject = {
+    host: host.value,
+    milliseconds: parseInt(milliseconds.value)
+  };
+  hostInput.value = "";
+  millisecondsInput.value = "";
   browser.storage.local.get({ rules: [] })
     .then(data => {
-      next = [...data.rules];
+      next = [...data.rules].filter(r => r.host !== dataObject.host);
       next.push(dataObject);
-      browser.storage.local.set({ rules: next });
-    })
-})
+      browser.storage.local.set({ rules: next })
+        .then(() => refreshRuleDisplay());
+    });
+});
+
+function refreshRuleDisplay() {
+  const rulesDiv = document.getElementById("rules");
+
+  browser.storage.local.get({ rules: [] })
+    .then(data => {
+      rulesDiv.replaceChildren();
+      rules = data.rules;
+      for (const rule of rules) {
+        const ruleElement = document.createElement("p");
+        ruleElement.innerText = `${rule.host}: ${rule.milliseconds}`;
+        rulesDiv.appendChild(ruleElement);
+      }
+    });
+}
--- timer.js
+++ timer.js
@@ -1,119 +1,124 @@
-// Time limits. Maps host to allowed milliseconds of browsing time.
-const RULES = new Map([
-  ["www.noahpauls.com", 5000],
-  ["www.joshwcomeau.com", 10234.23090978627890],
-]);
+browser.storage.local.get({ rules: [] })
+  .then(data => {
+    // Retrieve rules from storage
+    const ruleData = data.rules;
+    const rules = new Map(ruleData.map(r => ([r.host, r.milliseconds])));
+    return rules;
+  })
+  .then(rules => {
 
-// Get the host and record the visit to it
-let host = window.location.host;
-recordLastSite(host);
+    // Get the host and record the visit to it
+    let host = window.location.host;
+    recordLastSite(host);
 
-const limit = RULES.get(host);
-let checker = undefined;
+    const limit = rules.get(host);
+    let checker = undefined;
 
-// Start timing the current page view
-let viewStartTime = undefined;
-if (document.visibilityState === "visible") {
-  activate();
-}
+    // Start timing the current page view
+    let viewStartTime = undefined;
+    if (document.visibilityState === "visible") {
+      activate();
+    }
 
-window.addEventListener("visibilitychange", event => {
-  if (document.visibilityState === "visible") {
-    activate();
-  } else if (document.visibilityState === "hidden") {
-    pause();
-  }
-});
-
-window.addEventListener("pageshow", event => {
-  if (document.visibilityState === "visible") {
-    activate();
-  }
-});
-
-window.addEventListener("pagehide", event => {
-  pause();
-});
-
-function recordLastSite(site) {
-  modifyStorage("lastSite", [], prev => {
-    let next = [...prev];
-    next.push(site);
-    return next;
-  });
-}
-
-function activate() {
-  if (!limit) {
-    return;
-  }
-  // Ensure a checker isn't already running.
-  // TODO: determine why this happens; probably due to shared context between
-  // content scripts.
-  if (checker !== undefined) {
-    return;
-  }
-  setViewStartTime();
-  checker = doSetInterval(() => {
-    browser.storage.local.get({ [`viewTime:${host}`]: 0 })
-      .then(viewTimeResult => {
-        const viewTime = viewTimeResult[`viewTime:${host}`];
-        const additionalTime = Date.now() - viewStartTime;
-        if (viewTime + additionalTime >= limit) {
-          pause();
-          console.log(`LIMIT REACHED FOR THIS SITE`);
-          blockSite();
-        }
-      });
-  }, 1000);
-}
-
-function pause() {
-  if (!limit) {
-    return;
-  }
-  setSiteViewTime();
-  clearInterval(checker);
-  checker = undefined;
-}
-
-function blockSite() {
-  window.location = browser.runtime.getURL("pages/blocked/blocked.html");
-}
-
-function setViewStartTime() {
-  viewStartTime = Date.now();
-}
-
-function setSiteViewTime() {
-  if (viewStartTime === undefined || viewStartTime === null) {
-    console.error(`cannot record site view time when start time is ${viewStartTime}`)
-    return
-  }
-
-  const viewTime = Date.now() - viewStartTime;
-  
-  modifyStorage(`viewTime:${host}`, 0, prev => {
-    return prev + viewTime;
-  });
-}
-
-function modifyStorage(key, defaultValue = null, update) {
-  let getArg = key;
-  if (defaultValue !== null) {
-    getArg = { [key]: defaultValue };
-  }
-  browser.storage.local.get(getArg)
-    .then(value => {
-      value[key] = update(value[key]);
-      return value;
-    })
-    .then(value => {
-      browser.storage.local.set(value);
+    window.addEventListener("visibilitychange", event => {
+      if (document.visibilityState === "visible") {
+        activate();
+      } else if (document.visibilityState === "hidden") {
+        pause();
+      }
     });
-}
 
-function doSetInterval(func, delay) {
-  func();
-  return setInterval(func, delay);
-}
+    window.addEventListener("pageshow", event => {
+      if (document.visibilityState === "visible") {
+        activate();
+      }
+    });
+
+    window.addEventListener("pagehide", event => {
+      pause();
+    });
+
+    function recordLastSite(site) {
+      modifyStorage("lastSite", [], prev => {
+        let next = [...prev];
+        next.push(site);
+        return next;
+      });
+    }
+
+    function activate() {
+      if (!limit) {
+        return;
+      }
+      // Ensure a checker isn't already running.
+      // TODO: determine why this happens; probably due to shared context between
+      // content scripts.
+      if (checker !== undefined) {
+        return;
+      }
+      setViewStartTime();
+      checker = doSetInterval(() => {
+        browser.storage.local.get({ [`viewTime:${host}`]: 0 })
+          .then(viewTimeResult => {
+            const viewTime = viewTimeResult[`viewTime:${host}`];
+            const additionalTime = Date.now() - viewStartTime;
+            if (viewTime + additionalTime >= limit) {
+              pause();
+              console.log(`LIMIT REACHED FOR THIS SITE`);
+              blockSite();
+            }
+          });
+      }, 1000);
+    }
+
+    function pause() {
+      if (!limit) {
+        return;
+      }
+      setSiteViewTime();
+      clearInterval(checker);
+      checker = undefined;
+    }
+
+    function blockSite() {
+      window.location = browser.runtime.getURL("pages/blocked/blocked.html");
+    }
+
+    function setViewStartTime() {
+      viewStartTime = Date.now();
+    }
+
+    function setSiteViewTime() {
+      if (viewStartTime === undefined || viewStartTime === null) {
+        console.error(`cannot record site view time when start time is ${viewStartTime}`)
+        return
+      }
+
+      const viewTime = Date.now() - viewStartTime;
+
+      modifyStorage(`viewTime:${host}`, 0, prev => {
+        return prev + viewTime;
+      });
+    }
+
+    function modifyStorage(key, defaultValue = null, update) {
+      let getArg = key;
+      if (defaultValue !== null) {
+        getArg = { [key]: defaultValue };
+      }
+      browser.storage.local.get(getArg)
+        .then(value => {
+          value[key] = update(value[key]);
+          return value;
+        })
+        .then(value => {
+          browser.storage.local.set(value);
+        });
+    }
+
+    function doSetInterval(func, delay) {
+      func();
+      return setInterval(func, delay);
+    }
+
+  })
