a6788fafc3506380649ad7e29ca8bb409927182b
Noah Pauls <noahmpauls@gmail.com>
2023-02-16 14:35:52 -0800

    feat!: add toolbar button with settings link in popup

---
+++ pages/popup/popup.js
@@ -0,0 +1,14 @@
+const linkContainer = document.getElementById("link-container");
+
+const settingsLink = document.createElement("a");
+settingsLink.innerText = "Settings";
+settingsLink.href = browser.runtime.getURL("pages/settings/settings.html");
+settingsLink.target = "_blank";
+
+
+settingsLink.addEventListener("click", function() {
+  // just calling window.close() opens the link in a new window
+  setTimeout(window.close, 1);
+});
+
+linkContainer.appendChild(settingsLink);
