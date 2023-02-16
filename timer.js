e5d228c7a3cf4b7fdebc461d02f3200a2d41e3f9
Noah Pauls <noahmpauls@gmail.com>
2023-02-15 17:37:23 -0800

    feat!: block hosts using rule map

--- timer.js
+++ timer.js
@@ -1,30 +1,38 @@
+// Time limits. Maps host to allowed milliseconds of browsing time.
+const RULES = new Map([
+  ["www.noahpauls.com", 5000],
+  ["www.joshwcomeau.com", 10234.23090978627890],
+]);
+
 // Get the host and record the visit to it
 let host = window.location.host;
-console.log(host);
 recordLastSite(host);
+
+const limit = RULES.get(host);
+let checker = undefined;
 
 // Start timing the current page view
 let viewStartTime = undefined;
 if (document.visibilityState === "visible") {
-  setViewStartTime();
+  activate();
 }
 
 window.addEventListener("visibilitychange", event => {
   if (document.visibilityState === "visible") {
-    setViewStartTime();
+    activate();
   } else if (document.visibilityState === "hidden") {
-    setSiteViewTime();
+    pause();
   }
 });
 
 window.addEventListener("pageshow", event => {
   if (document.visibilityState === "visible") {
-    setViewStartTime();
+    activate();
   }
 });
 
 window.addEventListener("pagehide", event => {
-  setSiteViewTime();
+  pause();
 });
 
 function recordLastSite(site) {
@@ -35,22 +43,70 @@ function recordLastSite(site) {
   });
 }
 
+function activate() {
+  if (!limit) {
+    return;
+  }
+  // Ensure a checker isn't already running.
+  // TODO: determine why this happens; probably due to shared context between
+  // content scripts.
+  if (checker !== undefined) {
+    return;
+  }
+  setViewStartTime();
+  checker = doSetInterval(() => {
+    browser.storage.local.get({ [`viewTime:${host}`]: 0 })
+      .then(viewTimeResult => {
+        const viewTime = viewTimeResult[`viewTime:${host}`];
+        const additionalTime = Date.now() - viewStartTime;
+        if (viewTime + additionalTime >= limit) {
+          pause();
+          console.log(`LIMIT REACHED FOR THIS SITE`);
+          setBlockPage(host, limit);
+          // window.location = "https://developer.mozilla.org";
+        }
+      });
+  }, 1000);
+}
+
+function pause() {
+  if (!limit) {
+    return;
+  }
+  setSiteViewTime();
+  clearInterval(checker);
+  checker = undefined;
+}
+
+// Replace page content with block page.
+// TODO: find a better way to create a block page that is unaffected by
+// existing styles on the page.
+function setBlockPage(host, limit) {
+  document.body.style.all = "unset";
+  document.body.style.fontFamily = "sans-serif";
+  document.body.style.display = "flex";
+  document.body.style.flexDirection = "column";
+  document.body.style.justifyContent = "center";
+  document.body.style.alignItems = "center";
+  document.body.style.height = "100%";
+  document.body.innerHTML = 
+`<h1>Blocked!</h1>
+<p>You can only browse ${host} for ${parseFloat((limit / 1000).toFixed(2))} seconds.</p>`
+}
+
 function setViewStartTime() {
-  console.log("start view time");
   viewStartTime = Date.now();
 }
 
 function setSiteViewTime() {
-  console.log("end view time");
   if (viewStartTime === undefined || viewStartTime === null) {
-    console.error(`cannot record site view time when start time is ${viewStarTime}`)
+    console.error(`cannot record site view time when start time is ${viewStartTime}`)
     return
   }
 
   const viewTime = Date.now() - viewStartTime;
   
   modifyStorage(`viewTime:${host}`, 0, prev => {
-    console.log(prev);
     return prev + viewTime;
   });
 }
@@ -68,4 +124,9 @@ function modifyStorage(key, defaultValue = null, update) {
     .then(value => {
       browser.storage.local.set(value);
     });
+}
+
+function doSetInterval(func, delay) {
+  func();
+  return setInterval(func, delay);
 }
