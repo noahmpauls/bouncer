3f20b1a55964b5addeda95904b12ec01b95b335a
Noah Pauls <noahmpauls@gmail.com>
2023-03-26 18:19:00 -0700

    fix: path alias resolution for tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig");
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ["<rootDir>/test/jest/setup.ts"]
+  setupFilesAfterEnv: ["<rootDir>/test/jest/setup.ts"],
+
+  // https://kulshekhar.github.io/ts-jest/docs/getting-started/paths-mapping/#jest-config-with-helper
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
