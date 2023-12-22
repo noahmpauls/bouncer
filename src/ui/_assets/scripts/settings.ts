8ba98a7761ce2a9f42af1139819867740d502ba1
Noah Pauls <noah@noahpauls.com>
2023-12-21 18:27:09 -0800

    feat: delete button on ui

--- src/ui/_assets/scripts/settings.ts
+++ src/ui/_assets/scripts/settings.ts
@@ -6,49 +6,106 @@ import { ScheduledLimit } from "@bouncer/enforcer";
 import { ExactHostnameMatcher } from "@bouncer/matcher";
 import { AlwaysBlock, ViewtimeCooldownLimit } from "@bouncer/limit";
 import { AlwaysSchedule, MinuteSchedule } from "@bouncer/schedule";
+import { BasicGuard, type IGuard } from "@bouncer/guard";
+import { CachedBouncerContext, type IBouncerContext } from "@bouncer/context";
 import { BasicPage } from "@bouncer/page";
 
-const bouncerData: IBouncerData = new StoredBouncerData(new BrowserStorage());
+const bouncerData: IBouncerContext = new CachedBouncerContext(new StoredBouncerData(new BrowserStorage()));
 
 const policiesEditor = document.getElementById("policies-editor")!;
 
-policiesEditor.addEventListener("click", event => {
-  event.preventDefault();
-  
-  // bouncerData.addPolicy(policy)
-  //   // TODO: formalize the policy refresh message to background script
-  //   .then(() => browser.runtime.sendMessage({ type: "REFRESH", time: new Date() }))
-  //   .then(() => refreshPolicyDisplay());
-});
-
 function refreshPolicyDisplay() {
-  bouncerData.getGuards()
+  bouncerData.guards()
     .then(guards => {
       policiesEditor.replaceChildren();
       if (guards.length === 0) {
         policiesEditor.innerHTML = "<p><i>No policies to display.</i></p>";
       }
       for (const guard of guards) {
-        const id = guard.id;
-        const policyString = JSON.stringify(guard.policy.toObject(), null, 2);
-
-        const policyEditorTemplate = `<h3>${guard.policy.name}</h3>
-        <pre><code>${policyString}</code></pre>`;
-        const policyElement = document.createElement("div");
-        policyElement.innerHTML = policyEditorTemplate;
-        policiesEditor?.appendChild(policyElement);
+        policiesEditor?.appendChild(createPolicyEditor(guard));
       }
     });
 }
 
-async function seedPolicies() {
-  const existingPolicies = await bouncerData.getGuards();
+function createPolicyEditor(guard: IGuard) {
+  const title = document.createElement("h3");
+  title.innerText = guard.policy.name;
 
-  if (existingPolicies.length > 0) {
+  const textarea = document.createElement("textarea");
+  const policyString = JSON.stringify(guard.policy.toObject(), null, 2);
+  textarea.value = policyString;
+  const lines = policyString.split("\n").length;
+  textarea.rows = lines;
+  textarea.cols = 80;
+  textarea.addEventListener("input", e => {
+    const lines = (e.target as HTMLTextAreaElement).value.split("\n").length;
+    textarea.rows = lines;
+  })
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", () => {
+    bouncerData.guards()
+      .then(guards => {
+        const i = guards.indexOf(guard);
+        guards.splice(i, 1);
+        bouncerData.persist();
+      }).then(() => {
+        sendRefreshMessage();
+        refreshPolicyDisplay();
+      });
+  });
+
+  const resetButton = document.createElement("button");
+  resetButton.innerText = "Reset";
+  resetButton.addEventListener("click", () => {
+    const policyString = JSON.stringify(guard.policy.toObject(), null, 2);
+    textarea.value = policyString;
+    const lines = policyString.split("\n").length;
+    textarea.rows = lines;
+  });
+
+  const submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.innerText = "Update";
+  submitButton.addEventListener("click", e => {
+    e.preventDefault();
+    try {
+      const updatedPolicy = deserializePolicy(JSON.parse(textarea.value) as any);
+      guard.policy = updatedPolicy;
+      bouncerData.persist().then(() => {
+        sendRefreshMessage();
+        refreshPolicyDisplay();
+      });
+    } catch {
+      console.error("could not deserialize input");
+    }
+  });
+
+  const form = document.createElement("form");
+  form.id = `policy-editor-${guard.id}`;
+  form.name = `policyEditor${guard.id}`;
+  form.classList.add("policy-editor");
+
+  form.appendChild(title);
+  form.appendChild(textarea);
+  form.appendChild(deleteButton);
+  form.appendChild(resetButton);
+  form.appendChild(submitButton);
+
+  return form;
+}
+
+async function seedPolicies() {
+  const existingGuards = await bouncerData.guards();
+
+  if (existingGuards.length > 0) {
     return;
   }
 
-  const policies: IPolicy[] = [
+  existingGuards.push(new BasicGuard(
+    "0",
+    true,
     new BasicPolicy(
       "Block HackerNews",
       new ExactHostnameMatcher("news.ycombinator.com"),
@@ -57,6 +114,11 @@ async function seedPolicies() {
         new AlwaysBlock(),
       ),
     ),
+    new BasicPage()
+  ));
+  existingGuards.push(new BasicGuard(
+    "1",
+    true,    
     new BasicPolicy(
       "Limit Noah Pauls",
       new ExactHostnameMatcher("www.noahpauls.com"),
@@ -65,12 +127,15 @@ async function seedPolicies() {
         new ViewtimeCooldownLimit(10000, 15000),
       ),
     ),
-  ];
+    new BasicPage()
+  ));
 
-  for (const policy of policies) {
-    await bouncerData.addPolicy(policy);
-  }
+  await bouncerData.persist();
 
+  sendRefreshMessage();
+}
+
+function sendRefreshMessage() {
   browser.runtime.sendMessage({ type: "REFRESH", time: new Date() });
 }
 
