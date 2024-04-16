69e4706ffa729debcf0622dd0b0988b0604a828a
Noah Pauls <noah@noahpauls.com>
2024-04-15 20:11:56 -0700

    improve blocked page

--- src/ui/_assets/scripts/blocked.ts
+++ src/ui/_assets/scripts/blocked.ts
@@ -1,3 +1,19 @@
 if (window.top !== window.self) {
   document.querySelector("body")?.classList.add("frame");
 }
+
+const queryParams = new URLSearchParams(window.location.search);
+
+const title = queryParams.get("title");
+const url = queryParams.get("url");
+
+const titleContainer = document.getElementById("title") as HTMLSpanElement;
+titleContainer.innerHTML = title !== null ? title : "this site"
+if (title !== null) {
+  titleContainer.style.fontWeight = 'bold';
+}
+
+const tryAgainButton = document.getElementById("try-again") as HTMLButtonElement;
+tryAgainButton.addEventListener("click", () => {
+  url !== null && location.replace(url);
+});
