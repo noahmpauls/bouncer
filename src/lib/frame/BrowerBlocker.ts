b2812a36be7f263d66ae8259b6f02728eaac8ffa
Noah Pauls <noah@noahpauls.com>
2024-04-23 21:32:50 -0700

    more robust block page query parameter construction

--- src/lib/frame/BrowerBlocker.ts
+++ src/lib/frame/BrowerBlocker.ts
@@ -3,8 +3,16 @@ import type { IBlocker } from "./types";
 
 export const BrowserBlocker: IBlocker = {
   block: () => {
+    const queryParams = new URLSearchParams();
     const url = location.toString();
+    if (url !== "") {
+      queryParams.append("url", url);
+    }
     const title = document.title;
-    location.replace(browser.runtime.getURL(`dist/ui/blocked/index.html?url=${url}&title=${title}`));
+    if (title !== "") {
+      queryParams.append("title", title);
+    }
+    const queryString = queryParams.size > 0 ? `?${queryParams.toString()}` : "";
+    location.replace(browser.runtime.getURL(`dist/ui/blocked/index.html${queryString}`));
   }
 }
